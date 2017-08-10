187176b2b5dc52ba69ea964d7a72d4b85a63e15c
Li Chen <chen1i@github.io>
2017-08-11 06:54:20 +0800

    add hero, bidirection binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Hero';
+  hero: Hero = { id: 1, name: 'Windstorm' };
+}
+
+export class Hero {
+  id: number;
+  name: string;
 }
